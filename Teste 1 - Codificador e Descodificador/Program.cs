3845481ab5279c1fd6bd1fc89754792e87ed9634
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-05 23:35:35 -0300

    Criação dos métodos de ordenação e re-organização do método retornaRespostaCodificada

--- Teste 1 - Codificador e Descodificador/Program.cs
+++ Teste 1 - Codificador e Descodificador/Program.cs
@@ -35,7 +35,84 @@ namespace Teste_1___Codificador_e_Descodificador
         #endregion
 
         #region Métodos
+        #region Ordenação
+        public int selectionSort(char[,] matrizFrase)
+        {
+            int ondeEstaPalavraCerta = 0; //guarda posição do indice de entrada
+            for (int linha = 0; linha < matrizFrase.GetLength(0); linha++)
+            {
+                for (int linhaMais1 = linha + 1; linhaMais1 < matrizFrase.GetLength(0); linhaMais1++)
+                {
+                    if (char.ToLower(matrizFrase[linha, 0]) > char.ToLower(matrizFrase[linhaMais1, 0]))
+                    {
+                        trocarLinha(linha, linhaMais1, matrizFrase);
+                        trocarIndice(linha, linhaMais1, ref ondeEstaPalavraCerta);
+                    }
+                    else if (char.ToLower(matrizFrase[linha, 0]) == char.ToLower(matrizFrase[linhaMais1, 0]))
+                        Recursivo(linha, linhaMais1, matrizFrase, 0, ref ondeEstaPalavraCerta);
+                    #region Não recursivo Comentado
+                    //  Não recursivo
+                    //    int indice = 1;
+                    //    do
+                    //    {
+                    //        if (char.ToLower(matrizFrase[linha, indice]) > char.ToLower(matrizFrase[linhaMais1, indice]))
+                    //        {
+                    //            trocarLinha(linha, linhaMais1, matrizFrase);
+                    //            trocarIndice(linha, linhaMais1, ref ondeEstaPalavraCerta);
+                    //        }
+                    //        else
+                    //            indice++;
+                    //    } while (char.ToLower(matrizFrase[linha, indice - 1]) == char.ToLower(matrizFrase[linhaMais1, indice - 1]) && indice < matrizFrase.GetLength(1));
+                    //}
+                    #endregion
+                }
+            }
+            return ondeEstaPalavraCerta;
+        }
+        private void Recursivo(int linha1, int linha2, char[,] matrizFrase, int indice, ref int ondeEstaPalavraCerta)
+        {
+            if (indice > matrizFrase.GetLength(1) - 1)
+            {
+                //Base
+            }
+            else if (char.ToLower(matrizFrase[linha1, indice]) > char.ToLower(matrizFrase[linha2, indice]))
+            {
+                trocarLinha(linha1, linha2, matrizFrase);
+                trocarIndice(linha1, linha2, ref ondeEstaPalavraCerta);
+            }
+            else if (char.ToLower(matrizFrase[linha1, indice]) == char.ToLower(matrizFrase[linha2, indice]))
+                Recursivo(linha1, linha2, matrizFrase, indice + 1, ref ondeEstaPalavraCerta);
 
+        }
+        private void trocarLinha(int linha1, int linha2, char[,] matrizFrase)
+        {
+            for (int coluna = 0; coluna < matrizFrase.GetLength(1); coluna++)
+            {
+                char aux = matrizFrase[linha1, coluna];
+                matrizFrase[linha1, coluna] = matrizFrase[linha2, coluna];
+                matrizFrase[linha2, coluna] = aux;
+            }
+        }
+        private void trocarIndice(int linha1, int linha2, ref int ondeEstaPalavraCerta)
+        {
+            if (linha1 == ondeEstaPalavraCerta)
+                ondeEstaPalavraCerta = linha2;
+            else
+                ondeEstaPalavraCerta = linha1;
+        }
+        #endregion
+        public void escreverMatriz()
+        {
+            //para testes
+            for (int linha = 0; linha < matrizFrase.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matrizFrase.GetLength(1); coluna++)
+                {
+                    Console.Write(matrizFrase[linha, coluna]);
+                }
+                Console.WriteLine();
+            }
+        }
         protected virtual void criacaoMatriz()
         {
             //será implementado nas classes derivadas
@@ -56,10 +133,22 @@ namespace Teste_1___Codificador_e_Descodificador
         #endregion
 
         #region Métodos
+        public string retornaRespostaCodificada()
+        {
+            int indiceEntrada;
+
+            criacaoMatriz();
+
+            //começa ordenação
+            indiceEntrada = 0;
+
+            //["nomeCodificado", Indice utilizado]
+            return "[\'" + retornaUltimaColuna() + "\', " + indiceEntrada + "]";
+        }
         #region Primeira Etapa
         protected override void criacaoMatriz()
         {
-            //preenchimento da matriz 
+            //preenchimento da matriz usando shiftRightLogical
             char[] encode = frase.ToCharArray();
 
             for (int linha = 0; linha < MatrizFrase.GetLength(0); linha++)
@@ -91,18 +180,6 @@ namespace Teste_1___Codificador_e_Descodificador
                 teste[posicao] = MatrizFrase[posicao, MatrizFrase.GetLength(1) - 1];
 
             return new string(teste);
-        }
-        public string retornaRespostaCodificada()
-        {
-            int indiceEntrada;
-
-            criacaoMatriz();
-
-            //começa ordenação
-            indiceEntrada = 0;
-
-            //["nomeCodificado", Indice utilizado]
-            return "[\'" + retornaUltimaColuna() + "\', " + indiceEntrada + "]";
         }
         #endregion
         #endregion
