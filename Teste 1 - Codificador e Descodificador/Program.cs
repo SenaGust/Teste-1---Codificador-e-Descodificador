8f159349b10464ed5e75aaa7965fa18d2f4812c7
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-05 22:41:31 -0300

    Criação da Classe Codificacao com seus métodos e atributos

--- Teste 1 - Codificador e Descodificador/Program.cs
+++ Teste 1 - Codificador e Descodificador/Program.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Teste_1___Codificador_e_Descodificador
 {
+    class Codificacao
+    {
+        #region Atributos
+        public string frase;
+        private char[,] matrizFrase;
+        #endregion
+
+        #region Construtor
+        public Codificacao(string frase)
+        {
+            this.frase = frase;
+            matrizFrase = new char[frase.Length, frase.Length];
+        }
+        #endregion
+    }
+   
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
